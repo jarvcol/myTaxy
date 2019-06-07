d2755cefdd08754cfed62dd21f5a82a4a3a58a20
Jorge Andres Rojas Valencia <JARV@JARVs-MacBook-Pro.local>
2019-06-06 22:50:22 -0500

    Changing json array related methods from traditional loop search to stream. :)

--- src/test/java/test/utils/JsonUtilities.java
+++ src/test/java/test/utils/JsonUtilities.java
@@ -4,44 +4,37 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.*;
+import java.util.stream.StreamSupport;
+import static java.util.stream.Collectors.toList;
 
 public class JsonUtilities {
 
     public static List<Integer> getListOfIntegerValuesFromList(String parameterName, JSONArray objectList){
-        List<Integer> intList = new ArrayList<Integer>();
-        for (int i = 0; i < objectList.length(); i++){
-            intList.add(objectList.getJSONObject(i).getInt(parameterName));
-        }
-        return intList;
+        return StreamSupport.stream(objectList.spliterator(), false)
+                .map(JSONObject.class::cast)
+                .map(o -> o.getInt(parameterName))
+                .collect(toList());
     }
 
     public static List<String> getListOfStringValuesFromList(String parameterName, JSONArray objectList){
-        List<String> stringlist = new ArrayList<String>();
-        for (int i = 0; i < objectList.length(); i++){
-            stringlist.add(objectList.getJSONObject(i).getString(parameterName));
-        }
-        return stringlist;
+        return StreamSupport.stream(objectList.spliterator(), false)
+                .map(JSONObject.class::cast)
+                .map(o -> o.getString(parameterName))
+                .collect(toList());
     }
 
     public static JSONObject getObjectFromListByParameter(String parameterName, String parameterValue, JSONArray userList){
-        for (int i = 0; i < userList.length(); i++){
-            if(compareObjectByParameter(parameterName, parameterValue, userList.getJSONObject(i)))
-                return userList.getJSONObject(i);
-        }
-        return null;
+        Optional<JSONObject> matchingObject = StreamSupport.stream(userList.spliterator(), false)
+                .map(JSONObject.class::cast)
+                .filter(o -> o.getString(parameterName).equals(parameterValue))
+                .findFirst();
+        return matchingObject.orElse(null);
     }
 
     public static Collection<Object[]> getIterableFromList(List list){
         Collection<Object[]> objList = new ArrayList<Object[]>();
         list.forEach(item -> objList.add(new Object[]{item}));
         return objList;
-    }
-
-    private static boolean compareObjectByParameter(String parameterName, String parameterValue, JSONObject obj){
-        if (obj.getString(parameterName).equals(parameterValue))
-            return true;
-        else
-            return false;
     }
 }
 
